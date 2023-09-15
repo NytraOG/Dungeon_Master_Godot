148d3de4e74e8f0fa31227419d44e2554604aef7
Christian Nytra <ch.nytra.13@gmail.com>
2023-09-15 21:31:20 +0200

    Init

---
+++ Keys.cs
@@ -0,0 +1,8 @@
+﻿namespace DungeonMaster;
+
+public static class Keys
+{
+    public const string Esc  = "open_menu";
+    public const string I    = "open_inventory";
+    public const string Num7 = "attack";
+}

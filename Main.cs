f3d84b14f0860cf52cd33248ca5bf9a09648beb6
Christian Nytra <ch.nytra.13@gmail.com>
2023-09-16 22:15:08 +0200

    Buttons mit mainscript verknüppt, inb4 battlecontroller migration

---
+++ Main.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace DungeonMaster;
+
+public partial class Main : Node
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+
+    private void _on_start_round_pressed()
+ {
+ }
+
+private void _on_undo_pressed()
+{
+}
+}

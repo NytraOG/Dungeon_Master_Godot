a7e4c898615eea31600c4e9067fdcc9701f3c4b2
Christian Nytra <ch.nytra.13@gmail.com>
2023-09-16 12:56:20 +0200

    charge and attack animationprocess fertig

--- Character.cs
+++ Character.cs
@@ -1,49 +1,53 @@
+using System;
 using Godot;
 
 namespace DungeonMaster;
 
 public partial class Character : CharacterBody3D
 {
-    private Vector3 destination;
-    private bool    stopped;
-    private Vector3 targetVelocity = Vector3.Zero;
+    private AnimatedSprite3D animationSprite;
+    private Vector3          destination;
+    private bool             hasAttacked;
+    private Vector3          start;
+    private bool             stopped;
+    private Vector3          targetVelocity = Vector3.Zero;
 
     [Export]
     public int Speed { get; set; } = 14;
 
-    public override void _Ready() => destination = Position + new Vector3(4, 0, 0);
+    public override void _Ready()
+    {
+        animationSprite           = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
+        animationSprite.Animation = "idle";
+        destination               = Position + new Vector3(4, 0, 0);
+        start                     = Position;
+    }
 
     public override void _PhysicsProcess(double delta)
     {
-        var animationSprite = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
-
-        if (!stopped)
+        if (Input.IsActionPressed(Keys.Num7) && !animationSprite.IsPlaying())
         {
             animationSprite.Animation = "charge";
             animationSprite.Play();
+
+            var direction = new Vector3(1, 0, 0);
+            targetVelocity.X = direction.X * Speed;
+            targetVelocity.Y = direction.Y * Speed;
         }
 
-        if (Input.IsActionPressed(Keys.Num7))
+        if (Math.Abs(destination.X - Position.X) < .1 && !hasAttacked)
         {
+            targetVelocity = Vector3.Zero;
+
             animationSprite.AnimationLooped += () =>
             {
                 animationSprite.Stop();
+                animationSprite.Animation = "idle";
             };
 
             animationSprite.Animation = "attack";
-            animationSprite.Play();
-            stopped = !stopped;
+            hasAttacked               = !hasAttacked;
         }
-
-        if (destination.Length() > Position.Length())
-        {
-            Velocity = Vector3.Zero;
-            return;
-        }
-
-        var direction = new Vector3(1, 0, 0);
-        targetVelocity.X = direction.X * Speed;
-        targetVelocity.Y = direction.Y * Speed;
 
         Velocity = targetVelocity;
 

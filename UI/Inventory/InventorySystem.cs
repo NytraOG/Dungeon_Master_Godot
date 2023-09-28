337dad9d98c10f0665380c39df1789ef7e546976
Christian Nytra <ch.nytra.13@gmail.com>
2023-09-28 21:48:16 +0200

    Inventorysystem rein

---
+++ UI/Inventory/InventorySystem.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace DungeonMaster.UI.Inventory;
+
+public partial class InventorySystem : Node3D
+{
+	public InventoryItemSlot[] Slots;
+
+	public void Initialize(int inventorySize)
+	{
+
+	}
+	public override void _Process(double delta)
+	{
+	}
+}

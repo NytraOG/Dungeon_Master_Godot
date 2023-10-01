6e00b5365ada3589af6bac5327e1b33cca571a74
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-01 22:30:01 +0200

    heiltrank

---
+++ Models/Items/Consumables/HealthPotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DungeonMaster.Models.Items.Consumables;
+
+public class HealthPotion : BaseConsumable
+{
+    public int AmountHealed { get; set; }
+
+    public override void Use(BaseUnit actor) => throw new NotImplementedException();
+
+    public override void Consume(BaseUnit imbiber)
+    {
+        if (imbiber.CurrentHitpoints + AmountHealed > imbiber.MaximumHitpoints)
+            imbiber.CurrentHitpoints = imbiber.MaximumHitpoints;
+        else
+            imbiber.CurrentHitpoints += AmountHealed;
+    }
+}

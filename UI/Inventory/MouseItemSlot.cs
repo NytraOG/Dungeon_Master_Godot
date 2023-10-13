82b010119e1defe70fea004b4db76cdc3060bfb3
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-13 11:07:44 +0200

    mousItem

--- UI/Inventory/MouseItemSlot.cs
+++ UI/Inventory/MouseItemSlot.cs
@@ -1,6 +1,10 @@
-﻿namespace DungeonMaster.UI.Inventory;
+﻿using DungeonMaster.Models.Items;
+
+namespace DungeonMaster.UI.Inventory;
 
 public class MouseItemSlot
 {
-    
+
+    public int      CurrentStacksize { get; set; }
+    public BaseItem ContainedItem    { get; set; }
 }

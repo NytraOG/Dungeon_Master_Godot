bcf2fc4b1176a63df3359c1d3d8c1b817f5bab61
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-13 11:06:42 +0200

    ok

---
+++ Models/Items/Consumables/ArmorPotion.cs
@@ -0,0 +1,8 @@
+﻿namespace DungeonMaster.Models.Items.Consumables;
+
+public class ArmorPotion : BaseConsumable
+{
+    public override void Use(BaseUnit actor) => throw new System.NotImplementedException();
+
+    public override void Consume(BaseUnit imbiber) => throw new System.NotImplementedException();
+}

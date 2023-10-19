4e07a1e4447083d2423901b72d8f1cd52686baf6
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-19 22:11:03 +0200

    slottypes da

---
+++ Enums/EquipSlot.cs
@@ -0,0 +1,17 @@
+﻿namespace DungeonMaster.Enums;
+
+public enum EquipSlot
+{
+    Head,
+    Neck,
+    Shoulders,
+    Chest,
+    Wrist,
+    Gloves,
+    Belt,
+    Underpants,
+    Pants,
+    Socks,
+    Shoes,
+    Ring
+}
---
+++ UI/Inventory/EquipmentItemSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DungeonMaster.Enums;
+using Godot;
+
+namespace DungeonMaster.UI.Inventory;
+
+public class EquipmentItemSlot : PanelContainer,
+                                 INotifyPropertyChanged
+{
+    [Export]
+    public EquipSlot SlotType { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}

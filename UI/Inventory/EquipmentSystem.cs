df91c595b31677e2800eb0e427547ce1275410c1
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-22 21:44:46 +0200

    inb4 double refactor, statsDisplays generieren sich gut

--- UI/Inventory/EquipmentSystem.cs
+++ UI/Inventory/EquipmentSystem.cs
@@ -1,18 +1,24 @@
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using Godot;
 
 namespace DungeonMaster.UI.Inventory;
 
 public partial class EquipmentSystem : PanelContainer
 {
-    private double inventoryDisplayCooldown;
-    private Main   main;
+    private Label         heronameLabel;
+    private double        inventoryDisplayCooldown;
+    private Main          main;
+    private VBoxContainer statDisplayNode;
 
     public override void _Ready()
     {
         if (GetTree().CurrentScene is not Main mainScene)
             return;
+
+        statDisplayNode = GetNode<VBoxContainer>("Background/MarginContainer/HBoxContainer/Control/VBoxContainer/ScrollContainer/MarginContainer/StatsDisplay");
+        heronameLabel   = GetNode<Label>("Background/MarginContainer/HBoxContainer/Control/VBoxContainer/TextureRect/HeroName");
 
         main                 =  mainScene;
         main.PropertyChanged += UpsertStatsDisplayEntries;
@@ -20,15 +26,14 @@ public partial class EquipmentSystem : PanelContainer
 
     private void UpsertStatsDisplayEntries(object sender, PropertyChangedEventArgs args)
     {
-        if (args.PropertyName != nameof(Main.SelectedHero) ||  main.SelectedHero is null)
+        if (args.PropertyName != nameof(Main.SelectedHero) || main.SelectedHero is null)
             return;
 
-        var heronameLabel = GetNode<Label>("Background/MarginContainer/HBoxContainer/Control/VBoxContainer/TextureRect/HeroName");
         heronameLabel.Text = main.SelectedHero.Name;
 
         var statsDisplayEntryScrene = ResourceLoader.Load<PackedScene>("res://UI/Inventory/stats_display_entry.tscn");
-        var publicProperties        = main.SelectedHero.GetType().GetProperties();
-        var entries                 = this.GetAllChildren<HBoxContainer>();
+        var publicProperties        = main.SelectedHero.GetType().GetProperties().Where(pi => (pi.PropertyType == typeof(int) || pi.PropertyType == typeof(float)) && !pi.Name.Contains("Process"));
+        var entries                 = statDisplayNode.GetAllChildren<HBoxContainer>();
 
         foreach (var propertyInfo in publicProperties)
         {
@@ -38,10 +43,14 @@ public partial class EquipmentSystem : PanelContainer
             {
                 entry      = statsDisplayEntryScrene.Instantiate<HBoxContainer>();
                 entry.Name = new StringName(propertyInfo.Name);
+
+                statDisplayNode.AddChild(entry);
             }
 
-            var valueNode = entry.GetNode<Label>("Name");
-            valueNode.Text = propertyInfo.GetValue(main.SelectedHero)?.ToString();
+            var nameNode = entry.GetNode<Label>("Name");
+            nameNode.Text = propertyInfo.Name;
+            var valueNode = entry.GetNode<Label>("Value");
+            valueNode.Text = ((int)propertyInfo.GetValue(main.SelectedHero)!).ToString();
         }
     }
 

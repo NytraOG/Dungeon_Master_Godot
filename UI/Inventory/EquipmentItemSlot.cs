b25dbec0d94033e4d8082da5895f766f60bebebb
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-22 19:01:37 +0200

    equipmentslots kopf zeigt defaulticon nice

--- UI/Inventory/EquipmentItemSlot.cs
+++ UI/Inventory/EquipmentItemSlot.cs
@@ -1,25 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using DungeonMaster.Enums;
+using DungeonMaster.Models.Items.Equipment;
 using Godot;
 
 namespace DungeonMaster.UI.Inventory;
 
 public partial class EquipmentItemSlot : PanelContainer,
-                                 INotifyPropertyChanged
+                                         INotifyPropertyChanged
 {
     [Export]
     public EquipSlot SlotType { get; set; }
 
-    public TextureRect Icon { get; set; }
+    [Export]
+    public Texture2D DefaultIcon { get; set; }
 
+    public BaseEquipment                     EquipedItem { get; set; }
+    public TextureRect                       Icon        { get; set; }
     public event PropertyChangedEventHandler PropertyChanged;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    public override void _Ready()
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        Icon         = GetNode<TextureRect>("TextureRect");
+        Icon.Texture = DefaultIcon;
     }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
     protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
     {

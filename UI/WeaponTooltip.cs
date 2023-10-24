b5230abf634fd8db5a0e4d5a8ca216af579e126a
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-24 11:35:11 +0200

    tooltip hacks

--- UI/WeaponTooltip.cs
+++ UI/WeaponTooltip.cs
@@ -6,10 +6,7 @@ public partial class WeaponTooltip : PanelContainer
 {
     private Vector2 originalSize;
 
-    public override void _Ready()
-    {
-        originalSize = Size;
-        SetSize(new Vector2(originalSize.X, 350));
-    }
+    public override void _Ready() => originalSize = Size;
 
+    public override void _Process(double delta) => SetSize(new Vector2(originalSize.X, 400)); //Hack, aus irgendwelche gründen wächst der Tooltip von alleine auf 2800+ px, wtf
 }

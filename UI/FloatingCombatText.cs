5564243005881123bfdb108b7a552989defbe1c2
Christian Nytra <ch.nytra.13@gmail.com>
2023-10-26 14:37:11 +0200

    fct entsorgt sich selber jetzt

--- UI/FloatingCombatText.cs
+++ UI/FloatingCombatText.cs
@@ -4,14 +4,29 @@ namespace DungeonMaster.UI;
 
 public partial class FloatingCombatText : Node2D
 {
-    public Label Display { get; set; }
-    public int   Damage  { get; set; }
+    [Export]
+    public float DriftVelocity { get; set; } = 0.5f;
+
+    public Label  Display { get; set; }
+    public int    Damage  { get; set; }
+    public double Elapsed { get; set; }
 
     public override void _Ready()
     {
         Display      = GetNode<Label>("Label");
-        Display.Text = Damage.ToString("N");
+        Display.Text = Damage.ToString();
     }
 
-    public override void _Process(double delta) => Position += new Vector2(0, -1);
+    public override void _Process(double delta)
+    {
+        Elapsed += delta;
+
+        if (Elapsed >= 1)
+            Modulate = new Color(Modulate, (float)Elapsed - 1);
+
+        if (Elapsed >= 2)
+            QueueFree();
+
+        Position += new Vector2(0, -DriftVelocity);
+    }
 }

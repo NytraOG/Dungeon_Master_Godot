b676aa8dd29529f044b796ee519355058cda3772
Christian Nytra <ch.nytra.13@gmail.com>
2023-11-11 21:05:47 +0100

    müll raus

--- UI/Inventory/SkillselectionSlot.cs
+++ UI/Inventory/SkillselectionSlot.cs
@@ -4,9 +4,5 @@ namespace DungeonMaster.UI.Inventory;
 
 public class SkillselectionSlot : Control
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
 }

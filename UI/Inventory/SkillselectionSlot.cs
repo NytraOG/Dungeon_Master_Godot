e83c017fd7909058694775cb46d1f26125a02178
Christian Nytra <ch.nytra.13@gmail.com>
2023-11-17 16:14:52 +0100

    das bestimmt auch

--- UI/Inventory/SkillselectionSlot.cs
+++ UI/Inventory/SkillselectionSlot.cs
@@ -5,4 +5,6 @@ namespace DungeonMaster.UI.Inventory;
 public class SkillselectionSlot : Control
 {
     public override void _Ready() => base._Ready();
+
+    public override void _Process(double delta) => base._Process(delta);
 }

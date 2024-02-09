c885c7cc9d39e92f5fb41abadf6824f7479f083a
Christian Nytra <ch.nytra.13@gmail.com>
2024-02-09 15:00:05 +0100

    Shabby pants implementiert

--- Models/Items/Equipment/ShabbyPants.cs
+++ Models/Items/Equipment/ShabbyPants.cs
@@ -1,18 +1,16 @@
-using Godot;
-using System;
 using DungeonMaster.Enums;
-using DungeonMaster.Models;
-using DungeonMaster.Models.Items.Equipment;
+
+namespace DungeonMaster.Models.Items.Equipment;
 
 public partial class ShabbyPants : BaseArmor
 {
-	public override string FluffContent => "Worn and tattered, these pants are barely holding on.";
-	public override string FluffAuthor { get; }
-	public override string FluffDate { get; }
-	public override void Use(BaseUnit actor)
-	{
-		
-	}
+    public override string FluffContent => "Worn and tattered, these pants are barely holding on.";
+    public override string FluffAuthor { get; }
+    public override string FluffDate { get; }
 
-	public override EquipSlot EquipSlot => EquipSlot.Pants;
+    public override EquipSlot EquipSlot => EquipSlot.Pants;
+
+    public override void Use(BaseUnit actor)
+    {
+    }
 }

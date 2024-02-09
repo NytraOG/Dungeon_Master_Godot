ff385b90559ececf85661f7603db73d1891716cd
Christian Nytra <ch.nytra.13@gmail.com>
2024-02-09 11:40:07 +0100

    icon ressourcen rein da

--- Models/Items/Equipment/ShabbyPants.cs
+++ Models/Items/Equipment/ShabbyPants.cs
@@ -6,7 +6,7 @@ using DungeonMaster.Models.Items.Equipment;
 
 public partial class ShabbyPants : BaseArmor
 {
-	public override string FluffContent { get; }
+	public override string FluffContent => "Worn and tattered, these pants are barely holding on.";
 	public override string FluffAuthor { get; }
 	public override string FluffDate { get; }
 	public override void Use(BaseUnit actor)
